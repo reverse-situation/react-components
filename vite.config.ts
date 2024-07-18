22396ac361b054fe44064594bf0a6cab6834905d
Lucaus <mzx@go-goal.com>
2024-07-18 19:44:28 +0800

    test: 🚨 测试 build 打包发布

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: join(__dirname, './src/index.ts'),
+      name: 'test',
+      fileName: 'index',
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
